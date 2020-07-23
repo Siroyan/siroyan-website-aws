9d4e0f8cbdb765bf89e9af4220c13fe563b89daa
Siroyan <1517053@ed.tus.ac.jp>
2020-07-23 10:06:22 +0000

    [add] s3-deploy

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deploy
+)
+import os
 
 from siroyan_website.siroyan_website_stack import SiroyanWebsiteStack
 
 
 app = core.App()
-SiroyanWebsiteStack(app, "siroyan-website")
+SiroyanWebsiteStack(
+    app, "siroyan-website",
+    env = {
+        "region": os.environ["CDK_DEFAULT_REGION"],
+        "account": os.environ["CDK_DEFAULT_ACCOUNT"]
+    }
+)
 
 app.synth()
